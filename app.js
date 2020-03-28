666f66f66bb35b5489e6c9d3e9fd894e6a049dba
Huy Dinh <dinhquochuy.eeit@gmail.com>
2020-03-29 00:54:35 +0100

    Separate

--- app.js
+++ app.js
@@ -2,38 +2,6 @@
 
 const e = React.createElement;
 
-class CoronaCountByCountry extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.country = this.props.country;
-    this.state = {
-      coronaCases: 0,
-      some: ''
-    }
-    this.fetchNumberOfCases = this.fetchNumberOfCases.bind(this);
-    this.intervalID = setInterval(this.fetchNumberOfCases, 500);
-  }
-
-  fetchNumberOfCases() {
-    fetch('https://corona.lmao.ninja/countries/' + this.country)
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        this.setState({coronaCases: data.cases})
-      });
-  }
-
-  render() {
-    return React.createElement(
-      'h1',
-      {},
-      this.country + ' has ' + this.state.coronaCases + ' case(s) of COVID-19'
-    );
-  }
-}
-
 class App extends React.Component {
   constructor(props) {
     super(props);
